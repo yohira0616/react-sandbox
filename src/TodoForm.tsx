90f1b8c0102f60919aca5688fb0770073650abf5
yukito ohira <yukito.sys.so@gmail.com>
2019-02-07 11:49:41 +0900

    Todo Comonents

---
+++ src/TodoForm.tsx
@@ -0,0 +1,9 @@
+import React, {Component} from "react";
+
+export default class TodoForm extends Component {
+  render() {
+    return (
+      <div>Hello,React! This is TodoForm</div>
+    )
+  }
+}
---
+++ src/TodoList.tsx
@@ -0,0 +1,9 @@
+import React, {Component} from "react";
+
+export default class TodoList extends Component {
+  render() {
+    return (
+      <div>Hello,React! This is TodoList</div>
+    )
+  }
+}
