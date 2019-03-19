a816f8305a1f722548c129e7bef9d94134800bd9
yukito ohira <yukito.sys.so@gmail.com>
2019-03-19 14:05:14 +0900

    Actionを定義

---
+++ src/actions/CounterAction.ts
@@ -0,0 +1,5 @@
+interface CounterAction {
+  type: string
+}
+
+export default CounterAction
--- src/index.tsx
+++ src/index.tsx
@@ -6,9 +6,31 @@ import TodoForm from './TodoForm'
 import TodoList from './TodoList'
 import * as serviceWorker from './serviceWorker';
 import 'normalize.css'
+import {createStore} from "redux";
+import CounterAction from "./actions/CounterAction"
+
+function counter(state = 0, action:CounterAction) {
+  switch (action.type) {
+    case 'INCREMENT':
+      return state + 1
+    case 'DECREMENT':
+      return state - 1
+    default:
+      return state
+  }
+}
+
+let store = createStore(counter)
+
+store.subscribe(()=> console.log(store.getState()))
+
+store.dispatch({type:'INCREMENT'})
+store.dispatch({type:'INCREMENT'})
+store.dispatch({type:'DECREMENT'})
+
 
 //ReactDOM.render(<App />, document.getElementById('root'));
-ReactDOM.render(<TodoList />, document.getElementById('root'));
+ReactDOM.render(<TodoList/>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
