392d36aba7e42c30348695e3e48c5ba865d38bbb
yukito ohira <yukito.sys.so@gmail.com>
2019-07-20 11:45:54 +0900

    useMemoを使えるようになる

--- src/HookCounter.tsx
+++ src/HookCounter.tsx
@@ -1,14 +1,18 @@
-import { useCallback, useState } from "react";
+import { useCallback, useMemo, useState } from "react";
 import * as React from "react";
 
 const HookCounter: React.FC = () => {
   const [count, setCount] = useState(0);
+  const double = useMemo(() => count * 2, [count]);
+  const doubleWithUnit = useMemo(() => `${double} pt`, [double]);
   const handleClick = useCallback(() => {
-    setCount(count + 1);
-  }, [count]);
+    setCount(prev => prev + 1);
+  }, []);
   return (
     <div>
-      <p>{count}</p>
+      <p>count: {count}</p>
+      <p>double: {double}</p>
+      <p>doubleWithUnit:{doubleWithUnit}</p>
       <button onClick={handleClick}>+1</button>
     </div>
   );
