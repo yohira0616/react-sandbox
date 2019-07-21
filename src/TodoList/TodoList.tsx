26969230f39132c27c3f30b3d143337a2575ae8a
yukito ohira <yukito.sys.so@gmail.com>
2019-07-21 17:12:38 +0900

    function component化

--- src/TodoList/TodoList.tsx
+++ src/TodoList/TodoList.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { Component, useCallback, useState } from "react";
 import Todo from "./Todo";
 import TodoForm from "./TodoForm";
 
@@ -6,13 +6,50 @@ import TodoViewObject from "./TodoModel";
 import "./TodoList.css";
 import CounterButton from "../CounterButton";
 import store from "../stores/CounterStore";
-import {fetch} from "../api/TodoRepository"
-import HookCounter from "../HookCounter"
+import { fetch } from "../api/TodoRepository";
+import HookCounter from "../HookCounter";
 
 interface TodoListState {
   todos: Array<TodoViewObject>
 }
 
+const TodoList: React.FC = props => {
+  const [todos, setTodos] = useState<TodoViewObject[]>(fetch());
+
+  const removeTodo = useCallback((id: number) => {
+    setTodos(todos.filter(todo => todo.id !== id));
+  }, [todos]);
+
+  const addTodo = useCallback((name: string) => {
+    const newTodo = {
+      id: todos.length + 1,
+      name: name
+    };
+    todos.push(newTodo);
+    setTodos(Array.from(todos));
+  }, [todos]);
+
+  const renderTodo: JSX.Element[] = todos.map(todo => {
+    return <Todo id={todo.id} name={todo.name} onFinishButtonClick={removeTodo} key={todo.id}/>;
+  });
+
+  return (
+    <div className="container">
+      <h1>Hello React! This is TodoList</h1>
+      <TodoForm onSubmit={addTodo}/>
+      <ul className="todo-ul">
+        {renderTodo}
+      </ul>
+      <CounterButton/>
+      <div>カウント: {store.getState()}</div>
+      <HookCounter></HookCounter>
+    </div>
+  );
+};
+
+export default TodoList;
+
+/*
 export default class TodoList extends Component<{}, TodoListState> {
 
   constructor(props: {}) {
@@ -62,3 +99,4 @@ export default class TodoList extends Component<{}, TodoListState> {
     });
   }
 }
+ */
